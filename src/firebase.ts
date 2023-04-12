61f1e5d6df3516405893d6e121e01cabf1353376
Tao Jouet <taojouet@gmail.com>
2023-04-12 13:30:24 +0200

    add firebase connection

---
+++ src/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db : any = getFirestore(app);
+
+// Get a list of meals from your database
+async function getCities(db : any) {
+  const mealCollection = collection(db, 'meals');
+  const mealsSnapshot = await getDocs(mealCollection);
+  const mealList = mealsSnapshot.docs.map(doc => doc.data());
+  return mealList;
+}
